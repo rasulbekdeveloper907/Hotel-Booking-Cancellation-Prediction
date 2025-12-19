30e48a8bc6f3d0a1ab92dd5456714224c7d9d19f
Rasulbek <rassiazzi1892@gmail.com>
2025-12-19 18:17:54 +0500

    add1000

--- Source/preprosessing.py
+++ Source/preprosessing.py
@@ -1,61 +1,68 @@
-
 import pandas as pd
-
-class Cleaner:
-    def __init__(self, df):
-        self.df = df
-
-    def tozala(self):
-        for col in self.df.columns:
-            if self.df[col].isnull().any():
-                if self.df[col].dtype == 'object':
-                    self.df[col] = self.df[col].fillna(self.df[col].mode()[0])
-                else:
-                    self.df[col] = self.df[col].fillna(self.df[col].mean())
-        return self
-
-    def get_df(self):
-        return self.df
-    
-
-    
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-
-class Encoder:
-    def __init__(self, df):
-        self.df = df
-        self.encoder = LabelEncoder()
-
-    def encodla(self):
-        for col in self.df.columns:
-            if self.df[col].dtype == 'object':
-                if self.df[col].nunique() <= 5:
-                    dummies = pd.get_dummies(self.df[col], prefix=col, dtype=int)
-                    self.df = pd.concat([self.df.drop(columns=[col]), dummies], axis=1)
-                else:
-                    self.df[col] = self.encoder.fit_transform(self.df[col])
-        return self
-
-    def get_df(self):
-        return self.df 
-    
-    import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-class Scaler:
-    def __init__(self, df):
-        self.df = df
-        self.scaler = StandardScaler()
+class Cleaner:
+    def __init__(self):
+        self.fill_values = {}
 
-    def scaling_qil(self):
-        numeric_cols = self.df.select_dtypes(include=['int64']).columns
-        self.df[numeric_cols] = pd.DataFrame(
-            self.scaler.fit_transform(self.df[numeric_cols]),
-            columns=numeric_cols,
-            index=self.df.index
-        )
+    def fit(self, X):
+        for col in X.columns:
+            if X[col].dtype == 'object':
+                self.fill_values[col] = X[col].mode()[0]
+            else:
+                self.fill_values[col] = X[col].median()
         return self
 
-    def get_df(self):
-        return self.df
+    def transform(self, X):
+        X = X.copy()
+        for col, value in self.fill_values.items():
+            X[col] = X[col].fillna(value)
+        return X
+
+
+class Encoder:
+    def __init__(self, max_unique=5):
+        self.max_unique = max_unique
+        self.cat_cols = None
+        self.dummy_cols = {}
+
+    def fit(self, X):
+        self.cat_cols = X.select_dtypes(include='object').columns
+
+        for col in self.cat_cols:
+            if X[col].nunique() <= self.max_unique:
+                self.dummy_cols[col] = X[col].unique().tolist()
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        for col in self.cat_cols:
+            if col in self.dummy_cols:
+                for val in self.dummy_cols[col]:
+                    X[f"{col}_{val}"] = (X[col] == val).astype(int)
+                X.drop(columns=[col], inplace=True)
+            else:
+                X[col] = X[col].astype('category').cat.codes
+
+        return X
+    
+class Scaler:
+    def __init__(self):
+        self.scaler = StandardScaler()
+        self.num_cols = None
+
+    def fit(self, X):
+        self.num_cols = X.select_dtypes(include=['int64', 'float64']).columns
+        self.scaler.fit(X[self.num_cols])
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        X[self.num_cols] = self.scaler.transform(X[self.num_cols])
+        return X
+
+
+
+
+
